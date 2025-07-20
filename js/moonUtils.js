ddcb63a8d1d53c736ed5dbeee180fcf06250cc41
Mozuku (Ren) Kato <pjksanchez2010@gmail.com>
2025-07-20 11:43:50 -0700

    Update moonUtils.js

--- js/moonUtils.js
+++ js/moonUtils.js
@@ -1,1 +1,10 @@
-
+const phases = [
+  { name: "New Moon", img: "assets/moon-phase/new_moon.png", illum: "0%" },
+  { name: "Waxing Crescent", img: "assets/moon-phase/waxing_crescent.png", illum: "25%" },
+  { name: "First Quarter", img: "assets/moon-phase/first_quarter.png", illum: "50%" },
+  { name: "Waxing Gibbous", img: "assets/moon-phase/waxing_gibbous.png", illum: "75%" },
+  { name: "Full Moon", img: "assets/moon-phase/full_moon.png", illum: "100%" },
+  { name: "Waning Gibbous", img: "assets/moon-phase/waning_gibbous.png", illum: "75%" },
+  { name: "Last Quarter", img: "assets/moon-phase/last_quarter.png", illum: "50%" },
+  { name: "Waning Crescent", img: "assets/moon-phase/waning_crescent.png", illum: "25%" }
+];
